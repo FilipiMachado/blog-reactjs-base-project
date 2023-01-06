ae3fa689e85506eec807ee0efe14703e9e1ae8a2
fil <filipisee@hotmail.com>
2023-01-05 21:16:15 -0300

    configuring components && structuring react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,61 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Register from "../src/pages/Register";
+import Login from "../src/pages/Login";
+import WritePost from "../src/pages/WritePost";
+import Home from "../src/pages/Home";
+import SinglePost from "../src/pages/SinglePost";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post:id",
+        element: <SinglePost />,
+      },
+      {
+        path: "/write",
+        element: <WritePost />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
