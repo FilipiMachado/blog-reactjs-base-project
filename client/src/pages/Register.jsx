28320bb6d45f1734f364e00d96d749596dfbdd4e
fil <filipisee@hotmail.com>
2023-01-05 21:52:17 -0300

    finished register page structure/styles

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="auth">
+      <h1>Cadastro de uma nova conta</h1>
+      <form>
+        <input required type="text" placeholder="Digite o nome da conta" />
+        <input required type="email" placeholder="Digite o email" />
+        <input required type="password" placeholder="Digite a senha" />
+        <button>Cadastrar-se</button>
+        <p>Ocorreu um erro!</p>
+        <span>
+          Já tem uma conta? <Link to="/login">Faça Login</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
