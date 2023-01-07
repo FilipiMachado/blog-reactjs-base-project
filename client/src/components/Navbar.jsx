de9a1b83b184131339e218f6d8e028a5036fdf69
fil <filipisee@hotmail.com>
2023-01-06 21:35:52 -0300

    finished navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,21 +14,26 @@ const Navbar = () => {
           <Link className="link" to="?cat=art">
             <h6>ART</h6>
           </Link>
-          <Link className="link" to="?cat=art">
+          <Link className="link" to="?cat=science">
             <h6>SCIENCE</h6>
           </Link>
-          <Link className="link" to="?cat=art">
+          <Link className="link" to="?cat=technology">
             <h6>TECHNOLOGY</h6>
           </Link>
-          <Link className="link" to="?cat=art">
+          <Link className="link" to="?cat=cinema">
             <h6>CINEMA</h6>
           </Link>
-          <Link className="link" to="?cat=art">
+          <Link className="link" to="?cat=design">
             <h6>DESIGN</h6>
           </Link>
-          <Link className="link" to="?cat=art">
+          <Link className="link" to="?cat=food">
             <h6>FOOD</h6>
           </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">Escrever</Link>
+          </span>
         </div>
       </div>
     </div>
