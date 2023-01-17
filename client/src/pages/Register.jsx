bf97d584d398c4f66d7e4816216e881b08c42ec9
fil <filipisee@hotmail.com>
2023-01-16 21:46:29 -0300

    configuring submit inputs

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("");
+  };
+
+  console.log(inputs);
+
   return (
     <div className="auth">
       <h1>Cadastrar nova conta</h1>
       <form>
-        <input required type="text" placeholder="Digite o nome da conta" />
-        <input required type="email" placeholder="Digite o email" />
-        <input required type="password" placeholder="Digite a senha" />
-        <button>Cadastrar-se</button>
+        <input
+          required
+          type="text"
+          placeholder="Digite o nome da conta"
+          onChange={handleChange}
+          name="username"
+        />
+        <input
+          required
+          type="email"
+          placeholder="Digite o email"
+          name="email"
+        />
+        <input
+          required
+          type="password"
+          placeholder="Digite a senha"
+          name="password"
+        />
+        <button onClick={handleSubmit}>Cadastrar-se</button>
         <p>Ocorreu um erro!</p>
         <span>
           Já tem uma conta? <Link to="/login">Faça Login</Link>
