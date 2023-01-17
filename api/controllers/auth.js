ed111813dd4850a4efe45ed8fbd7250195e635b7
fil <filipisee@hotmail.com>
2023-01-16 21:19:49 -0300

    working on controllers

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
-import { db } from "../db";
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
   // CHECK EXISTING USER
@@ -6,6 +7,19 @@ export const register = (req, res) => {
 
   db.quer(q, [req.body.email, req.body.username], (err, data) => {
     if (err) return res.json(err);
+    if (data.length) return res.status(409).json("Usuário ja existe!");
+
+    // Hash the password and create a user
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("Novo usuário criado.");
+    });
   });
 };
 
